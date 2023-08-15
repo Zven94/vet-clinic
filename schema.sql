00d91c76366627b0951b6b9e39cc025173b322ef
Nicolas Bolañez <Zven94>
2023-08-15 18:47:27 -0300

    fix name variable

--- schema.sql
+++ schema.sql
@@ -3,8 +3,8 @@
 CREATE DATABASE vet_clinic;
 
 CREATE TABLE animals (
-    id SERIAL PRIMARY KEY,
-    name VERCHAR(255),
+    id INT GENERATED ALWAYS AS IDENTITY,
+    name VARCHAR(255),
     date_of_birth DATE,
     escape_attempts INTEGER,
     neutered BOOLEAN,
