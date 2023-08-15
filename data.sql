5e6e259943c1accf003086d5dbcd474911a2d2bc
Nicolas Bolañez <Zven94>
2023-08-15 18:48:56 -0300

    add data into the table; show how on data.sql

--- data.sql
+++ data.sql
@@ -1,5 +1,9 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+
+vet_clinic=# INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
+vet_clinic-# VALUES ('Agumon', '2020/02/03', 0, true, 10.23),
+vet_clinic-# ('Gabumon', '2018/11/15', 2, true, 8.00),
+vet_clinic-# ('Pikachu', '2021/01/07', 1, false, 15.04),
+vet_clinic-# ('Devimom', '2017/05/12', 5, true, 11.00);
+INSERT 0 4
