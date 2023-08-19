e67b93a5bcec2fa502734621b49dc285525da28f
Nicolas Bolañez <Zven94>
2023-08-19 10:13:54 -0300

    delete unnecessary command line

--- data.sql
+++ data.sql
@@ -1,11 +1,11 @@
 /* Populate database with sample data. */
 
 
-vet_clinic=# INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
-vet_clinic-# VALUES ('Agumon', '2020/02/03', 0, true, 10.23),
-vet_clinic-# ('Gabumon', '2018/11/15', 2, true, 8.00),
-vet_clinic-# ('Pikachu', '2021/01/07', 1, false, 15.04),
-vet_clinic-# ('Devimom', '2017/05/12', 5, true, 11.00);
+ INSERT INTO animals(name, date_of_birth, escape_attempts, neutered, weight_kg)
+ VALUES ('Agumon', '2020/02/03', 0, true, 10.23),
+ ('Gabumon', '2018/11/15', 2, true, 8.00),
+ ('Pikachu', '2021/01/07', 1, false, 15.04),
+ ('Devimom', '2017/05/12', 5, true, 11.00);
 INSERT 0 4
 
 
@@ -24,44 +24,44 @@ VALUES ('Charmander','2020/02/08',0,'false',-11,'Animal'),
 
 /*  Insert the following data into the owners table   */
 
-vet_clinic=# INSERT INTO owners (full_name,age)
-vet_clinic-# VALUES ('Sam Smith',19),
-vet_clinic-# ('Jennifer Orwell', 19),
-vet_clinic-# ('Bob',45),
-vet_clinic-# ('Melody Pond', 77),
-vet_clinic-# ('Dean Winchester', 14),
-vet_clinic-# ('Jodie Whittaker',38);
+ INSERT INTO owners (full_name,age)
+ VALUES ('Sam Smith',19),
+ ('Jennifer Orwell', 19),
+ ('Bob',45),
+ ('Melody Pond', 77),
+ ('Dean Winchester', 14),
+ ('Jodie Whittaker',38);
 
 /*  Fix SAM SMITH age   */
 
-vet_clinic=# UPDATE owners SET age = 34 WHERE full_name = 'Sam Smith';
+ UPDATE owners SET age = 34 WHERE full_name = 'Sam Smith';
 
 /*  Insert the following data into the species table  */
 
-vet_clinic=# INSERT INTO species (name)
-vet_clinic-# VALUES ('Pokemon'),
-vet_clinic-# ('Digimon');
+ INSERT INTO species (name)
+ VALUES ('Pokemon'),
+ ('Digimon');
 
 /*  Modify your inserted animals so it includes the species_id value  */
 
-vet_clinic=# UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Digimon') WHERE name NOT LIKE '%mon';
-vet_clinic=# UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') WHERE name NOT LIKE '%mon';
+ UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Digimon') WHERE name NOT LIKE '%mon';
+ UPDATE animals SET species_id = (SELECT id FROM species WHERE name = 'Pokemon') WHERE name NOT LIKE '%mon';
 
 /*  Modify your inserted animals to include owner information (owner_id)  */
 
-vet_clinic=# UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Agumon' AND o.full_name = 'Sam Smith';
+ UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Agumon' AND o.full_name = 'Sam Smith';
 
-vet_clinic=# UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Gabumon' AND o.full_name = 'Jennifer Orwell';
+ UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Gabumon' AND o.full_name = 'Jennifer Orwell';
 UPDATE 1
-vet_clinic=# UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Pikachu' AND o.full_name = 'Jennifer Orwell';
+ UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Pikachu' AND o.full_name = 'Jennifer Orwell';
 
-vet_clinic=# UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Devimon' AND o.full_name = 'Bob';
+ UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Devimon' AND o.full_name = 'Bob';
 UPDATE 1
-vet_clinic=# UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Plantmon' AND o.full_name = 'Bob';
+ UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name = 'Plantmon' AND o.full_name = 'Bob';
 
-vet_clinic=# UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name IN ('Charmander','Squirtle','Blossom') AND o.full_name = 'Melody Pond';
+ UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name IN ('Charmander','Squirtle','Blossom') AND o.full_name = 'Melody Pond';
 
-vet_clinic=# UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name IN ('Angemon','Boarmon') AND o.full_name = 'Dean Winchester';
+ UPDATE animals AS a SET owner_id = o.id FROM owners AS o WHERE a.name IN ('Angemon','Boarmon') AND o.full_name = 'Dean Winchester';
 
 /*  FOURTH PART   */
 
